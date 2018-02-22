1c7c179c7e2b79de4fe432d3cf88dff0d4b1453a
Evan <ejmeidell@gmail.com>
2018-02-21 22:53:11 -0700

    Added pest control page with basic content

--- src/pages/pestControl/index.js
+++ src/pages/pestControl/index.js
@@ -1,12 +1,39 @@
 import React from "react";
+import style from "styled-components";
+import BlockSection from "../../components/blockSection.js";
+import cheep from "../../assets/Cheep-cheep.png";
+import bobomb from "../../assets/Bob-omb-resize.png";
+import goomba from "../../assets/Goombas-resize.png";
+
+const Style = style.div`
+  position: relative;
+  margin: 0 20px 0 20px;
+
+  .title {
+    display: block;
+    margin: 40px 0 0 70px;
+    font-size: 45px;
+  }
+
+  .content-body {
+    margin-top: 20px;
+    display: flex;
+    width: 100%
+  }
+`;
 
 class PestControl extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Pest Control is here!</h1>
-        <div>Mario pest control payment managment.</div>
-      </div>
+      <Style>
+        <div className="title">Mario Pest Control</div>
+        <br/>
+        <div className="content-body">
+          <BlockSection sectionTitle="Bob-omb" img={bobomb}>Testing</BlockSection>
+          <BlockSection sectionTitle="Goomba" img={goomba}>Testing 2</BlockSection>
+          <BlockSection sectionTitle="Cheep-cheep" img={cheep}>Testing 3</BlockSection>
+        </div>
+      </Style>
     )
   }
 }
